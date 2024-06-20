385f92a8df1fa2fe70b45604bb467df87b70bb41
Daniel Su <daniel8su@outlook.com>
2024-06-20 17:45:45 -0400

    keys

--- keyboards/handwired/split72/keymaps/default/keymap.c
+++ keyboards/handwired/split72/keymaps/default/keymap.c
@@ -16,29 +16,29 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ├──────┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┐    ┌─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴────┬───┼───┤
     │ Shift  │ Z │ X │ C │ V │ B |    | B │ N │ M │ , │ . │ / │Shift │ ↑ │PDn|
     ├────┬───┴┬──┴─┬─┴──┬┴───┴───┤    ├───┴──┬┴───┼───┴┬──┴─┬─┴──┬───┼───┼───┤
-    │Ctrl│Win │Alt │ Fn |        |    |      │ Fn │Ctrl|Win |Alt | ← │ ↓ │ → │
+    │Ctrl│Win │Alt │ Fn |        |    |      │ Fn │Win |Ctrl|Alt | ← │ ↓ │ → │
     └────┴────┴────┴────┴────────┘    └──────┴────┴────┴────┴────┴───┴───┴───┘
     */
     [0] = LAYOUT_qwerty(
         KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,                      KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_DEL,  KC_GRV,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC, KC_BSLS,
-        KC_BSPC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,  KC_PGUP,
+        KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,  KC_PGUP,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                      KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,   KC_PGDN,
-        KC_LCTL, KC_LGUI, KC_LALT, MO(1),   KC_SPC,                                      KC_SPC,  MO(1),   KC_RCTL, KC_RGUI, KC_RALT, KC_LEFT, KC_DOWN, KC_RGHT
+        KC_LCTL, KC_LGUI, KC_LALT, MO(1),   KC_SPC,                                      KC_SPC,  MO(1),   KC_RGUI, KC_RCTL, KC_RALT, KC_LEFT, KC_DOWN, KC_RGHT
     ),
 
     [1] = LAYOUT_qwerty(
         KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  n______, n______,
-        n______, n______, KC_UP,   n______, n______, n______,                   n______, n______, KC_UP,   n______, n______, n______, n______, n______, n______,
-        KC_CAPS, KC_LEFT, KC_DOWN, KC_RGHT, n______, n______,                            n______, KC_LEFT, KC_RGHT, n______, n______, n______, n______, KC_HOME,
+        KC_CAPS, n______, KC_UP,   n______, n______, n______,                   n______, n______, KC_UP,   n______, n______, n______, n______, n______, n______,
+        KC_BSPC, KC_LEFT, KC_DOWN, KC_RGHT, n______, n______,                            n______, KC_LEFT, KC_RGHT, n______, n______, n______, n______, KC_HOME,
         n______, n______, n______, n______, n______, n______,                   n______, n______, KC_DOWN, n______, n______, n______, n______, n______, KC_END,
         n______, n______, n______, MO(2),   n______,                                     n______, MO(2),   n______, n______, n______, n______, n______, n______
     ),
 
     [2] = LAYOUT_qwerty(
         n______, n______, n______, n______, n______, n______, n______,                   n______, n______, n______, n______, n______, n______, n______, n______,
-        n______, n______, n______, n______, n______, n______,                   n______, n______, KC_PGUP, n______, n______, n______, n______, n______, n______,
-        KC_INS,  n______, n______, n______, n______, n______,                            n______, KC_HOME, KC_END,  n______, n______, n______, n______, n______,
+        KC_INS,  n______, n______, n______, n______, n______,                   n______, n______, KC_PGUP, n______, n______, n______, n______, n______, n______,
+        n______, n______, n______, n______, n______, n______,                            n______, KC_HOME, KC_END,  n______, n______, n______, n______, n______,
         n______, n______, n______, n______, n______, n______,                   n______, n______, KC_PGDN, n______, n______, n______, n______, n______, n______,
         n______, n______, n______, n______, n______,                                     n______, n______, n______, n______, n______, n______, n______, n______
     )
